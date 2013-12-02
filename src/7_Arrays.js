d5053ba8a044d3312009fa958251192659a9251f
Breno Ferreira <bferreira@windowslive.com>
2013-12-02 20:25:27 -0200

    fixed filter

--- src/7_Arrays.js
+++ src/7_Arrays.js
@@ -1,10 +1,14 @@
 ArrayOps = {
   map: function(f, array){
     if(array.length == 0) return [];
-    else return [f(array[0])].concat(ArrayOps.map(f, array.slice(1)));
+    else{
+      var res = f(array[0]);
+      if(res === void 0) return ArrayOps.map(f, array.slice(1));
+      else return [res].concat(ArrayOps.map(f, array.slice(1)));
+    }
   },
 
   filter: function(f, array){
-    return ArrayOps.map(function(n) { if (f(n)) return n; else return [] }, array);
+    return ArrayOps.map(function(n) { if (f(n)) return n; }, array);
   }
 }
