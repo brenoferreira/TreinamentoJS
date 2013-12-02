8ee803f9f08dfd682d1f862261ca88f70e1549c9
Breno Ferreira <bferreira@windowslive.com>
2013-12-02 13:02:44 -0200

    Objects

---
+++ spec/2_Objects.spec.js
@@ -0,0 +1,43 @@
+describe("Objects", function () {
+  it("ciaAerea", function(){
+    expect(Voo.ciaAerea).toBe("TAM");
+  });
+
+  it("voo", function(){
+    expect(Voo.voo).toBe("JJ1234");
+  });
+
+  it("origem", function(){
+    expect(Voo.origem.cidade).toBe("São Paulo");
+  });
+
+  it("destino", function(){
+    expect(Voo.destino.cidade).toBe("New York");
+  });
+
+  it("duas paradas", function(){
+    expect(Voo.paradas.length).toEqual(2);
+  });
+
+  it("primeira parada no panama", function(){
+    expect(Voo.paradas[0].cidade).toBe("Ciudad de Panama");
+  });
+
+  it("segunda parada em Miami", function(){
+    expect(Voo.paradas[1].cidade).toBe("Miami");
+  });
+
+  it("sem terceira parada", function(){
+    expect(Voo.paradas[3]).toBeUndefined();
+  });
+
+  it("obtem valor de propriedade com []", function(){
+    expect(Voo["destino"]["aeroporto"]).toBe("JFK - John F. Kennedy International Airport");
+  });
+
+  it("altera data de chegada", function(){
+    Voo.destino.horario = "10-13-2013 20:30";
+
+    expect(Voo.destino.horario).toBe("10-13-2013 20:30");
+  })
+});
---
+++ src/2_Objects.js
@@ -0,0 +1,26 @@
+Voo = {
+  ciaAerea: "TAM",
+  voo: "JJ1234",
+  origem: {
+    aeroporto :"GRU - Aeroporto Internacional de Guarulhos",
+    horario: "10-12-2013 15:30",
+    cidade: "São Paulo"
+  },
+  destino: {
+    aeroporto :"JFK - John F. Kennedy International Airport",
+    horario: "10-13-2013 19:30",
+    cidade: "New York"
+  },
+  paradas: [
+    {
+      aeroporto :"PTY - Tocumen International Airport",
+      horario: "10-13-2013 09:30",
+      cidade: "Ciudad de Panama"
+    },
+    {
+      aeroporto :"MIA - Miami International Airport",
+      horario: "10-13-2013 16:30",
+      cidade: "Miami"
+    }
+  ]
+}
